6e521e1cc689666aa9a177f13e6e6cc549eb1703
Hexx King <hexxking13@gmail.com>
2020-11-12 10:39:29 -0800

    working copy from JB's demo/explination

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -4,23 +4,60 @@ Welcome to my Madlib game!
 
 ''')
 
-# path = open('assets/game.txt')
-with open('assets/game.txt') as path:
-  string_template = path.readlines()
-print(string_template) #Test
-
-
 def read_template(path):
-    content = open(path).read()
-    return content.strip()
-print('hit here') #Test
-
+    with open(path) as file:
+      content = file.read()
+        stipped_content = content.strip()
+    return stripped_content
 
 def parse_template(string_template):
-    #remove these strings from the game.txt file
-    string_template.strip('\n', 'Adjective', 'A First Name', 'Past Tense Verb', 'Plural Noun', 'Large Animal', 'Small Animal', 'A girls Name', 'Number 1-50', 'Number')
-    return string_template
-print(string_template) #Test
+    capturing = False
+    dissected_string = ""
+    parts = []
+    current_speech_part = ""
+
+    # "A {Adjective} and {Adjective} {Noun}."
+    #  ^
+    #capturing = True
+    #dissected_string = "A {} and {} {}"
+    #current_speech_part = "Adjective" - should build up to Adjective, the add it to the parts list and var gets set back to 0 before the next speech part
+    #push to parts - ["Adjective", "Adjective", "Noun"]
+
+    for char in string_template:
+        if not capturing:
+            dissected_string += char
+
+            if char == "{"
+                capturing = True
+
+        else: 
+            if char == "}":
+                capturing = False
+                parts.append(current_speech_part)
+                dissected_string += char
+                current_speech_part = ""
+            else:
+                current_speech_part += char
+    return (dissected_string,parts) #could also be a list but tuple preferred
+
+
+    # dissected_string = "A {} and {} {}."
+    # parts = ["Adjective," "Adjective", "Noun"]
+    # return (dissected_string, parts)
+
+def merge(bare_template, responses):
+    return bare_template.format(*responses) # the * breaks up the list into individual args
+    
+    #alternative
+    # merged = bare_template
+    # for response in responses:
+    #     merged = merged.replace("{}", response, 1)
+    # return merged
+
+
+
+
+
 
 
 
